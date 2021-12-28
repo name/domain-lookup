00b46bc1ba7b84429dfb0777e50036b128d3ff45
charlie <charlie@maddex.co>
2021-12-28 21:43:18 +0000

    feat: example upload
    Signed-off-by: charlie <charlie@maddex.co>

--- example/example.rs
+++ example/example.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
-}
+extern crate domain_lookup;
+use domain_lookup::DomainLookup;
+
+let mut tree = DomainLookupTree::new();
+
+// Insert some domains
+tree.insert(".twitter.com");
+
+// Perform lookups
+tree.lookup("api.twitter.com");
